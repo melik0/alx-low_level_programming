82a0c82b736fde50ed2d0a81a9c49caab19d2cc8
melik0 <melikaman8@gmail.com>
2023-02-18 23:47:12 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *this function is used to print "\"Programming is like building a multilingual puzzle" using the puts function
+*
+*/
+ int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
+
+
