7970dfde30c054e83c3d003d9929b5eb97cff8ed
melik0 <melikaman8@gmail.com>
2023-02-20 10:55:54 -0800

    new addition

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,30 +1,32 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 /**
- * main - print number
+ * main - Prints if number is positive, zero or negative
  *
- * Return: always (success)
+ * Return: Always (Success)
  */
-	int main(void)
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
 	{
-		int n;
-
-		srand(time(0));
-		n = rand() - RAND_MAX / 2;
-
-			if (n > 0)
-			{
-				printf("%d is positive\n",n);
-			}
-			else if (n == 0)
-			{
-				printf("%d is zero\n",n);
-			}
-			else 
-			{
-				printf("%d is negative\n",n);
-			}
-		return (0);
+		printf("%d is positive\n", n);
 	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+
+	return (0);
+}
+
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,31 +1,33 @@
-#include <stdiio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 /**
- * main - last digit
+ * main - Prints a text according number
  *
- * Return: Always success
+ * Return: Always (Success)
  */
-
-	int main(void)
+int main(void)
 {
 	int n, lastd;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	lastd = n % 10;
-		if (lastd > 5)
-		{
-			printf("Last digit of %d is %d and is greater than 5", n, lastd);
-		}
-		else if (lastd == 0)
-		{
-			printf("Last digit of %d is %d and is 0", n, lastd);
-		}
-		else if (lastd < 6 && lastd != 0)
-		{
-			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
-		}
-			retrun(0);
+
+	if (lastd > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
+	}
+	else if (lastd == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, lastd);
+	}
+	else if (lastd < 6 && lastd != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
+	}
+
+	return (0);
 }
+
