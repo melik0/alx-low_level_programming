2259b818d3a626fbace65f2f1bc2b1774676ef78
melik0 <melikaman8@gmail.com>
2023-02-21 12:42:28 -0800

    new addition

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints _putchar
+ *
+ * Return: always success
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char ch = 'a';
+		while (ch <= 'z')
+		{
+			putchar(ch);
+			ch++;
+		}
+		putchar('\n');
+
+			return (0);
+}
